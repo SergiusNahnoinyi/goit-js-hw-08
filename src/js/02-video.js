807a71e2f44a6d40ed11947dca3f287d9cda2559
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-20 16:52:53 +0200

    Инициализирует плеер в файле скрипта

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+'use strict';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
