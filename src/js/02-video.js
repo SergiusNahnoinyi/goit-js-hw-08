1b519a364d4a1e55578b66c65ad8b42a7f9c462a
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-20 17:07:51 +0200

    Отслеживает событие timeupdate

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,3 +6,9 @@ const player = new Vimeo.Player(iframe);
 player.on('play', function () {
   console.log('played the video!');
 });
+// You can listen for events in the player by attaching a callback using .on():
+player.on('timeupdate', function (event) {
+  console.log(event.seconds);
+  console.log(event.percent);
+  console.log(event.duration);
+});
