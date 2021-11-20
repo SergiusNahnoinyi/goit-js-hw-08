5650fa3298f6d5b8d7bf700fd5be9518bc7ac2c2
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-20 18:05:50 +0200

    Добавляет метод воспроизведения с сохраненной позиции

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,6 +3,7 @@
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const timeWatched = localStorage.getItem(LOCALSTORAGE_KEY);
 
 player.on('play', function () {
   console.log('played the video!');
@@ -17,3 +18,20 @@ player.on('timeupdate', function (event) {
   console.log(timeUpdate);
   localStorage.setItem(LOCALSTORAGE_KEY, timeUpdate.seconds);
 });
+
+player
+  .setCurrentTime(timeWatched)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        'the time was less than 0 or greater than the video’s duration';
+        break;
+
+      default:
+        'some other error occurred';
+        break;
+    }
+  });
