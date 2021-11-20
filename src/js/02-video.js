21195bd2c46c9096a11de3dd220f55c274cc2bb9
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-20 17:23:53 +0200

    Сохраняет время воспроизведения в локальное хранилище

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,18 @@
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 player.on('play', function () {
   console.log('played the video!');
 });
 // You can listen for events in the player by attaching a callback using .on():
 player.on('timeupdate', function (event) {
-  console.log(event.seconds);
-  console.log(event.percent);
-  console.log(event.duration);
+  const timeUpdate = {
+    seconds: event.seconds,
+    percent: event.percent,
+    duration: event.duration,
+  };
+  console.log(timeUpdate);
+  localStorage.setItem(LOCALSTORAGE_KEY, timeUpdate.seconds);
 });
