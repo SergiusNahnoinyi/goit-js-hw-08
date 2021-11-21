7b9dae5d9263947c2278f026f281a8cb3b2c010e
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-21 16:07:27 +0200

    Отслеживает на форме событие input, и каждый раз записывает в локальное хранилище объект с полями email и password

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+'use strict';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', saveFormData);
+
+function saveFormData(event) {
+  const formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
