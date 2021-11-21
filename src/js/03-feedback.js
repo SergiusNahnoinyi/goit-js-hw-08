78127841e426b40b1b94d4086f21971aa19f8d75
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-21 16:59:18 +0200

    Проверяет состояние хранилища, и если там есть сохраненные данные, заполняет ими поля формы

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,6 +6,8 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', saveFormData);
 form.addEventListener('submit', onFormSubmit);
 
+checkFormData();
+
 function saveFormData(event) {
   const formData = {
     email: form.elements.email.value,
@@ -18,4 +20,13 @@ function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   console.log(localStorage.getItem(LOCALSTORAGE_KEY));
+}
+
+function checkFormData() {
+  const savedFormData = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+  if (parsedFormData) {
+    form.elements.email.value = parsedFormData.email;
+    form.elements.message.value = parsedFormData.message;
+  }
 }
