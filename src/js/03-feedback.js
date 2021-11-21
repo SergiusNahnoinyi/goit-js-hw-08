77481b1d56779f332fbe68cf9007ab9c1b4ff6dc
Sergius Nahnoinyi <sergiusnahnoinyi@gmail.com>
2021-11-21 16:38:09 +0200

    При сабмите формы очищает хранилище и поля формы, а также выводит объект в консоль

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,6 +4,7 @@ const LOCALSTORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', saveFormData);
+form.addEventListener('submit', onFormSubmit);
 
 function saveFormData(event) {
   const formData = {
@@ -11,4 +12,10 @@ function saveFormData(event) {
     message: form.elements.message.value,
   };
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(localStorage.getItem(LOCALSTORAGE_KEY));
 }
